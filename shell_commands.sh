221e6eea4af6ceb9c4241c0205c74f1a3561ca45
mehmetsekeroglu <mehmetsekeroglu2019@gmail.com>
2020-12-20 22:07:33 +0100

    shell_commands created

---
+++ shell_commands.sh
@@ -0,0 +1,16 @@
+Linux  Komutlari
+
+1) pwd          :   hangi dosyada bulundugunuzu gosterir.
+2) ls -a        :   gizli dosyalar dahil tüm dosyalari listeler.
+3) mkdir        :   yeni klasor acar.
+4) touch        :   dosya olusturur.
+5) cp           :   bir klasore dosya kopyalar.
+6) cd           :   bir klasore gecis yapilir.
+7) rm           :   bir dosyayi siler.
+8) echo         :   bir dosya acip icine yazi yazilir.
+9) cat          :   dosyanin icerigi gösterilir.
+10) grep        :   dosyada arama yapilir.   
+11) cat         :   iki dosya birlestirilir.   
+12) cd ..       :   bir ust dizine cikilir.
+13) rm -r       :   dolu olan klasör icidekiler ile birlikte silinir.
+14) sudo su     :   root a gecis yapilir
