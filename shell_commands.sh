dbf0ca5da9e187384166972d714300c2cc313d9c
mehmetsekeroglu <mehmetsekeroglu2019@gmail.com>
2020-12-20 22:09:58 +0100

    commands update

--- shell_commands.sh
+++ shell_commands.sh
@@ -1,13 +1,13 @@
 Linux  Komutlari
 
 1) pwd          :   hangi dosyada bulundugunuzu gosterir.
-2) ls -a        :   gizli dosyalar dahil tüm dosyalari listeler.
+2) ls -a        :   gizli dosyalar dahil tüm dosyalari listelenir.
 3) mkdir        :   yeni klasor acar.
 4) touch        :   dosya olusturur.
-5) cp           :   bir klasore dosya kopyalar.
-6) cd           :   bir klasore gecis yapilir.
-7) rm           :   bir dosyayi siler.
-8) echo         :   bir dosya acip icine yazi yazilir.
+5) cp           :   klasore dosya kopyalar.
+6) cd           :   klasore gecis yapilir.
+7) rm           :   dosyayi siler.
+8) echo         :   dosya acip icine yazi yazilir.
 9) cat          :   dosyanin icerigi gösterilir.
 10) grep        :   dosyada arama yapilir.   
 11) cat         :   iki dosya birlestirilir.   
