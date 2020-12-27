8f138d82fcc754b7ffc5862cc44c722ead0de1f7
mehmetsekeroglu <mehmetsekeroglu2019@gmail.com>
2020-12-27 20:26:33 +0100

    added packagers-editor

---
+++ linux-commands-packagers-editors.js
@@ -0,0 +1,31 @@
+/**
+ * sudo apt update      :   Günceleme yapilir.
+ * sudo apt upgrade     :   Kurulu programlar güncellenir.
+ * sudo apt install --classic code          :    Visual Studio Code programi kurulur.
+ * wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb   : chrome indirilir. 
+ * sudo apt install ./google-chrome-stable_current_amd64.deb                        : chorme yüklenir.
+ * sudo apt install libreoffice             :    LibreOffice yüklenir.
+ * LibreOffice programinda dosyalar         :    "Writer" ile Word (.docx) formatinda
+ *                                               "Calc" ile Excel (.xlsx) formatinda,
+ *                                               "Impress" ile yeni bir belgeyi Powerpoint (.pptx) formatinda kaydedilir.
+ * sudo adduser kullanici2                  :   Bu komut ile adi kullanici2  olan yeni bir kullanici tanimlanir.
+ * sudo addgroup hicoders                   :   hicoders adinda bir grup olusturulur.
+ * vim index.html                           :   index.html dosyasi olusturulur "i" ile etkinlestirilir. "esc" ve ":wq" ile kaydedip cikilir.
+ * nano app.js                              :   app.js dosyasi olusturulur. "ctrl o" ile kaydedilir. "ctrl x" ile cikis yapilir.
+ * sudo chown kullanici2:hicoders ./app.js  : yetki verilir.
+ * chmod u=rwx og=rwx index.html            :   Okuma, yazma ve çalistirma izinleri verilir.
+ * ls-l                                     :   Degisiklikler kontrol edilir. 
+ * alias        :   Kisayol atamasi yapar.
+ * df           :   Hafiza kullanimini gösterir.
+ * history      :   Gecmiste kullanilan kodlari gösterir.
+ * date         :   Saat ve tarih bilgisi verir.
+ * ps           :   Bilgisayarin halihazirda yapilan islemleri gösterir.
+ * kill         :   Bir programi kapatir.
+ * tar          :   Zip komutudur.
+ * top          :   CPU kullanimini gösterir.
+ * du           :   Icinde bulunulan klasörün kapladigi alani gösterir.
+ * head         :   Bir belgenin ilk 10 satirini gösterir.
+ * tail         :   Bir belgenin son 10 satirini gösterir.
+ * wget         :   Terminal üzerinden internetten indirme islemi yapilmasini saglar.
+ * find         :   Arama yapilmasini saglar.
+ */
